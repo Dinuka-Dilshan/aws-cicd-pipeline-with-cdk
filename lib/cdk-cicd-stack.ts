02efff7a2c682b40fd5984775e67308fd3b60c05
Dinuka Dilshan <jadinukadilshan@gmail.com>
2024-10-22 16:17:58 +0530

    fix

--- lib/cdk-cicd-stack.ts
+++ lib/cdk-cicd-stack.ts
@@ -1,8 +1,23 @@
 import * as cdk from "aws-cdk-lib";
+import {
+  CodePipeline,
+  CodePipelineSource,
+  ShellStep,
+} from "aws-cdk-lib/pipelines";
 import { Construct } from "constructs";
 
 export class CdkCicdStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+
+    const pipeline = new CodePipeline(this, "Pipeline", {
+      synth: new ShellStep("Synth", {
+        input: CodePipelineSource.gitHub(
+          "Dinuka-Dilshan/aws-cicd-pipeline-with-cdk",
+          "master"
+        ),
+        commands: ["npm ci", "npx cdk synth", ""],
+      }),
+    });
   }
 }
